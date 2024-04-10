4fe329079f346b1d640cc9b7209b342f74b2e1f9
Popsyynn <popsyynn@gmail.com>
2024-04-10 01:05:42 -0700

    Created a new class : Rectangle

---
+++ src/Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle implements Shape{
+    @Override
+    public int getNumberOfEdges() {
+        return 4;
+    }
+
+    @Override
+    public int getNumberOfVertices() {
+        return 4;
+    }
+}
