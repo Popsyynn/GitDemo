b4f79d75355e49f1fb2c8d3f684d815fae9ed024
Popsyynn <popsyynn@gmail.com>
2024-04-10 00:59:55 -0700

    Created a new class : Triangle

---
+++ src/Triangle.java
@@ -0,0 +1,11 @@
+public class Triangle implements Shape{
+    @Override
+    public int getNumberOfEdges() {
+        return 4;
+    }
+
+    @Override
+    public int getNumberOfVertices() {
+        return 5;
+    }
+}
