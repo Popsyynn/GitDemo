5b6e17796cd5c3a33ed12c7d82db93d0d01830f5
Popsyynn <popsyynn@gmail.com>
2024-04-10 00:55:28 -0700

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,11 @@
+public class Circle implements Shape{
+    @Override
+    public int getNumberOfEdges() {
+        return 1;
+    }
+
+    @Override
+    public int getNumberOfVertices() {
+        return 2;
+    }
+}
---
+++ src/Square.java
@@ -0,0 +1,11 @@
+public class Square implements Shape{
+    @Override
+    public int getNumberOfEdges() {
+        return 4;
+    }
+
+    @Override
+    public int getNumberOfVertices() {
+        return 3;
+    }
+}
